1e3f8b81b3c3eb885eea7855a1c755c492fb16bd
martanetza <39929716+martanetza@users.noreply.github.com>
2019-02-27 09:55:33 +0100

    correcting errors and adding first and last names

--- javascript3.js
+++ javascript3.js
@@ -1,15 +1,14 @@
 "use strict";
-const baseLink = "http://petlatkea.dk/2019/students1991.json";
+const baseLink = "https://petlatkea.dk/2019/students1991.json";
 const studentObject = {
   fullname: "-student name-",
-  house: "-student house-"
+  house: "-student house-",
+  firstname: "-student first name-",
+  lastname: "-student last name-"
 };
 const arrayOfStudents = [];
 let filter;
-init();
 function init() {
-  console.log("init");
-
   getJSON();
 
   document.querySelectorAll("p").forEach(elm => {
@@ -25,21 +24,24 @@ function getJSON() {
 }
 function makeObject(studentList) {
   studentList.forEach(item => {
+    const firstSpace = item.fullname.indexOf(" ");
+    const lastSpace = item.fullname.lastIndexOf(" ");
+
     const newObject = Object.create(studentObject);
     newObject.fullname = item.fullname;
     newObject.house = item.house;
+    newObject.firstname = item.fullname.slice(0, firstSpace);
+    newObject.lastname = item.fullname.slice(lastSpace + 1);
 
     arrayOfStudents.push(newObject);
   });
+  console.log(arrayOfStudents);
 
   filterList();
 }
 function filterList() {
-  arrayOfStudents.filter(filterOne);
   const filterdList = arrayOfStudents.filter(filterOne);
-  // console.log(filterdList);
-
-  display(filterdList);
+  console.log(filterdList);
 
   sortList(filterdList);
 }
@@ -48,9 +50,19 @@ function sortList(filterdList) {
   document.querySelector("#sortBy").addEventListener("change", sortingOption);
   sortingOption();
   function sortingOption() {
-    if (document.querySelector("select").value === "name") {
+    if (document.querySelector("select").value === "firstName") {
       filterdList.sort(function(a, b) {
-        if (a.fullname < b.fullname) {
+        if (a.firstname < b.firstname) {
+          return -1;
+        } else {
+          return +1;
+        }
+      });
+      display(filterdList);
+    }
+    if (document.querySelector("select").value === "lastName") {
+      filterdList.sort(function(a, b) {
+        if (a.lastname < b.lastname) {
           return -1;
         } else {
           return +1;
@@ -94,6 +106,9 @@ function display(filterdList) {
   document.querySelectorAll(".name").forEach(item => {
     item.remove();
   });
+  document.querySelectorAll(".lastname").forEach(item => {
+    item.remove();
+  });
   document.querySelectorAll(".place").forEach(item => {
     item.remove();
   });
@@ -101,7 +116,8 @@ function display(filterdList) {
     let template = document.querySelector("template");
 
     const copy = template.cloneNode(true).content;
-    copy.querySelector(".name").textContent = item.fullname;
+    copy.querySelector(".name").textContent = item.firstname;
+    copy.querySelector(".lastname").textContent = item.lastname;
     copy.querySelector(".place").textContent = item.house;
     document.querySelector("table").appendChild(copy);
   });
